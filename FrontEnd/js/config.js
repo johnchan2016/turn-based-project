ac0a67b7aa64e2d203ec565449e61e6465d42219
johnchan <myhk2009@gmail.com>
2021-08-08 17:16:17 +0800

    update config

--- FrontEnd/js/config.js
+++ FrontEnd/js/config.js
@@ -6,6 +6,6 @@ window._config = {
     },
     api: {
         //invokeUrl: 'https://91jwj31mt4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-        invokeUrl: 'http://localhost:8081'
+        invokeUrl: 'http://localhost:8080'
     }
 };
