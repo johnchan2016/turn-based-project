278673b7790de603dc347c775ff39e10944c34eb
johnchan <myhk2009@gmail.com>
2021-09-19 22:28:52 +0800

    update

--- scripts/update-yaml-values.sh
+++ scripts/update-yaml-values.sh
@@ -2,6 +2,6 @@
 
 printenv | sort
 
-yq eval '.image.tag = $IMAGE_TAG' backend-charts/api/values.yaml > backend-charts/api/values2.yaml
+yq eval '.image.tag = dev-202109192228' backend-charts/api/values.yaml > backend-charts/api/values2.yaml
 
 cat backend-charts/api/values2.yaml
