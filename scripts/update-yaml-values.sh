501e39996f4f9d2ec0dc18af168db4f147293fe0
johnchan <myhk2009@gmail.com>
2021-09-19 21:07:25 +0800

    update

--- scripts/update-yaml-values.sh
+++ scripts/update-yaml-values.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-ls backend-charts
 ls backend-charts/api
-echo 'env.CurrentHelmPath: ' + env.CurrentHelmPath
+echo 'env.CurrentHelmPath: ${CurrentHelmPath}'
 
-if [ env.CurrentHelmPath == '' ]
+if [ env.CurrentHelmPath = "" ]
 then
   yq eval '.image.tag = "${CurrentEnv}-${CurrentTimestamp}"' backend-charts/api/values.yaml
 else
-  path="backend-charts/api/values-${CurrentHelmPath}.yaml"
+  path="backend-charts/api/values_${CurrentHelmPath}.yaml"
   yq eval '.image.tag = "${CurrentEnv}-${CurrentTimestamp}"' ${path}
 fi
 
