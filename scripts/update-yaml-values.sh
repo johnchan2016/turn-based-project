2aff7c119d0f19e2043ae3bcde85f40ffa816e71
johnchan <myhk2009@gmail.com>
2021-09-19 23:02:05 +0800

    update

--- scripts/update-yaml-values.sh
+++ scripts/update-yaml-values.sh
@@ -2,6 +2,14 @@
 
 printenv | sort
 
-yq eval '.image.tag = "dev-202109192228"' $HELM_VALUE_PATH > $HELM_VALUE_PATH
+yq eval '.image.tag = "dev-202109192228"' $HELM_VALUE_PATH > backend-charts/api/values-new.yaml
 
+echo 'values-new.yaml'
+cat backend-charts/api/values-new.yaml
+
+rm $HELM_VALUE_PATH
+
+mv  backend-charts/api/values-new.yaml > $HELM_VALUE_PATH
+
+echo 'backend-charts/api/values-new.yaml'
 cat $HELM_VALUE_PATH
