9f34d0a97f9e0786e38ae5f87e72a9e8f1cca305
johnchan <myhk2009@gmail.com>
2021-09-22 00:11:28 +0800

    test

--- scripts/remove-unused-value-files.sh
+++ scripts/remove-unused-value-files.sh
@@ -7,7 +7,7 @@ echo 'list files'
 cd turn-based-api-chart
 ls
 
-find . -type f -name 'values*.txt' -delete
+find . -type f -name 'values*.yaml' -delete
 mv "new.yaml" "values.yaml"
 
 echo 'list files v2'
