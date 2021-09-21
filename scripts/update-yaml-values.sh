b3e9a1e956f6dc9b01140c6a09f912f537f93820
johnchan <myhk2009@gmail.com>
2021-09-21 23:51:46 +0800

    test

--- scripts/update-yaml-values.sh
+++ scripts/update-yaml-values.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yq eval '.image.tag = env(IMAGE_TAG)' $HELM_VALUE_PATH > backend-charts/api/values-new.yaml
+yq eval '.image.tag = env(IMAGE_TAG)' $HELM_VALUE_PATH > backend-charts/api/new.yaml
 
 echo 'remove old & replace new'
 rm $HELM_VALUE_PATH
-mv backend-charts/api/values-new.yaml $HELM_VALUE_PATH
+mv backend-charts/api/new.yaml $HELM_VALUE_PATH
