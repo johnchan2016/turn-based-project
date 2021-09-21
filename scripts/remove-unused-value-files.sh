2e5fa01d41c29cf5193e85cbdc0068dd4f9eed99
johnchan <myhk2009@gmail.com>
2021-09-21 23:43:56 +0800

    test

--- scripts/remove-unused-value-files.sh
+++ scripts/remove-unused-value-files.sh
@@ -6,14 +6,18 @@ cp -r backend-charts/api/* turn-based-api-chart
 echo 'list files'
 ls
 
-rm -v !("turn-based-api-chart/$HELM_VALUE_PATH")
-mv "turn-based-api-chart/$HELM_VALUE_PATH" "turn-based-api-chart/values.yaml"
+echo 'list files v2'
+cd turn-based-api-chart
+ls
 
-helm package turn-based-api-chart/*
-helm repo index --url https://github.com/johnchan2016/turn-based-helm-chart.git .
+# rm -v !("turn-based-api-chart/$HELM_VALUE_PATH")
+# mv "turn-based-api-chart/$HELM_VALUE_PATH" "turn-based-api-chart/values.yaml"
 
-echo '***** get content of index.yaml *****' 
-cat index.yaml
-git add . && \
-git commit -m "create helm chart for version $IMAGE_TAG" && \
-git push origin master
+# helm package turn-based-api-chart/*
+# helm repo index --url https://github.com/johnchan2016/turn-based-helm-chart.git .
+
+# echo '***** get content of index.yaml *****' 
+# cat index.yaml
+# git add . && \
+# git commit -m "create helm chart for version $IMAGE_TAG" && \
+# git push origin master
