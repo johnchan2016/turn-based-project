24e2ebe53d3fa490dc9740983185819aa4fd6469
johnchan <myhk2009@gmail.com>
2021-09-24 19:43:39 +0800

    update

--- scripts/remove-helm-chart-folder.sh
+++ scripts/remove-helm-chart-folder.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ -d "$HUDSON_HOME/workspace/turn-based-helm-chart" ] 
+if [ -d "$HUDSON_HOME/workspace/turn-based-helm-chart/" ] 
 then
     rm -rf "$HUDSON_HOME/workspace/turn-based-helm-chart"
-    ls
-    HELM_CHART_HOME="$HUDSON_HOME/workspace/turn-based-helm-chart"
 fi
