e0c084afe913050f3c50681ab2ac3a6853bca083
johnchan <myhk2009@gmail.com>
2021-09-24 19:21:23 +0800

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,6 +20,7 @@ pipeline {
         IMAGE_TAG = "$CurrentEnv-$CurrentTimestamp";
         HELM_VALUE_FILE = GetHelmValueFile(CurrentEnv);
         HELM_VALUE_PATH = "backend-charts/api/$HELM_VALUE_FILE";
+        HELM_CHART_HOME = ""
     }
 
     agent {
--- scripts/remove-helm-chart-folder.sh
+++ scripts/remove-helm-chart-folder.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ -d "turn-based-helm-chart" ] 
+if [ -d "$HUDSON_HOME/workspace/turn-based-helm-chart" ] 
 then
     rm -rf "$HUDSON_HOME/workspace/turn-based-helm-chart"
-    export HELM_CHART_HOME="$HUDSON_HOME/workspace/turn-based-helm-chart"
+    ls
+    HELM_CHART_HOME="$HUDSON_HOME/workspace/turn-based-helm-chart"
 fi
